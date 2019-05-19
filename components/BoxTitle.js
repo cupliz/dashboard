e39aebaf158bae674323d73e23f9743ce956add3
iyan <iyan.cupliz@gmail.com>
2019-05-19 14:35:46 +0700

    fixing style

--- components/BoxTitle.js
+++ components/BoxTitle.js
@@ -8,16 +8,16 @@ class BoxTitle extends React.Component {
       <div className="card">
         <div className="card-body row pr-4">
           <div className=" col-lg-3 col-xl-2 col-12 col-md-3 bg-transparent mt-1">
-            <img src="../static/image/imageDarktrace.jpeg " class="card-img-top rounded m-0" />
+            <img src="../static/image/imageDarktrace.jpeg " className="card-img-top rounded m-0" />
           </div>
           <div className="col p-0 m-0 bg-transparent mr-auto mb-2 mt-2">
             <h5 className="card-title m-0 col bg-transparent">Darktrace</h5>
             <p className="m-0 col bg-transparent">www.darktrace.com</p>
             <div className="row m-0 col bg-transparent">
                 <img src="../static/image/facebook.png" className="img m-1"/>
-                <img src="../static/image/twitter.png"  className="img m-1"/>
-                <img src="../static/image/instagram.jpg"   className="img m-1"/>
-                <img src="../static/image/linkedin.png"  className="img m-1"/>
+                <img src="../static/image/twitter.png" className="img m-1"/>
+                <img src="../static/image/instagram.jpg" className="img m-1"/>
+                <img src="../static/image/linkedin.png" className="img m-1"/>
             </div>
           </div>
           <h6 className="col-12 col-sm-12 col-lg-3 col-xl-3 rounded-pill text-center private" >Private</h6>
