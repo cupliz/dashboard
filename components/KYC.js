6e5a2dd8c5125ef3d8598bee7ebbee3b28aa1e60
muhyidin3222 <muhyidin480@gmail.com>
2019-05-19 14:22:10 +0700

    add 4 card

--- components/KYC.js
+++ components/KYC.js
@@ -1,18 +1,40 @@
 import React from "react";
 
 class KYC extends React.Component {
-  componentDidMount() {}
+  componentDidMount() { }
 
   render() {
     return (
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">KYC</h5>
-          <p>
-          This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 className="card-title pl-3">KYC</h5>
+          <div className="row text-center">
+            <div className="col-3 ">
+              <div className="pb-2 text-left pl-2">Watchlist</div>
+              <div className="rounded-pill bg-success"></div>
+            </div>
+            <div className="col-3 border-left border-dark ">
+              <div className="pb-2">Regulatory</div>
+              <div className="rounded-pill col bg-warning"></div>
+            </div>
+            <div className="col-3 border-right border-left border-dark">
+              <div className="pb-2">Litigation</div>
+              <div className="rounded-pill col bg-danger"></div>
+            </div>
+            <div className="col-3">
+              <div className="pb-2"> Media</div>
+              <div className="rounded-pill bg-success"></div>
+            </div>
+          </div>
         </div>
-        <div className="card-footer">
-          Last updated 3 mins ago
+        <div className="card-footer m-0 pb-0">
+          <div className="row text-secondary text-center p-0 m-0 pb-3">
+            <p className="col text-light bg-transparent">Reports</p>
+            <p className="col bg-transparent "><u>Director</u></p>
+            <p className="col bg-transparent"><u>Consumer</u></p>
+            <p className="col bg-transparent"><u>Trace</u></p>
+            <p className="col bg-transparent"><u>AML</u></p>
+          </div>
         </div>
       </div>
     );
