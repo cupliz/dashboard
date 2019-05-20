b41e30f5763d56feecc858a26e97cd5821542c5d
muhyidin3222 <muhyidin480@gmail.com>
2019-05-20 07:12:05 +0700

    update FinancialMetrics

--- components/FinancialMetrics.js
+++ components/FinancialMetrics.js
@@ -1,57 +1,77 @@
+
+
+
 import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { columnOptions } from './Options'
 
 class FinancialMetrics extends React.Component {
   componentDidMount() { }
-
   render() {
+    const growthSummary = [
+      {
+        name: "",
+        year2017: '2017',
+        year2018: "2018",
+        chart: [],
+      },
+      {
+        name: "Turnovor",
+        year2017: '£30.759.000',
+        year2018: "£59.484.000",
+        chart: [10, -8, 10, 40, 30],
+      },
+      {
+        name: "Gress Profit Margin",
+        year2017: '76,6%',
+        year2018: "81,9%",
+        chart: [10, 50, 0, -30, 20],
+      },
+      {
+        name: "Net Income Morgin",
+        year2017: '(80,9%)',
+        year2018: "(66,1%)",
+        chart: [10, 50, -10, 40, 20],
+      },
+      {
+        name: "Return on Equity",
+        year2017: '(178,3%)',
+        year2018: "(290,4%)",
+        chart: [10, 50, 10, 5, 10],
+      },
+      {
+        name: "Turnover/Employee",
+        year2017: '£52.941',
+        year2018: "£79,952",
+        chart: [10, -20, 0, 50, 10],
+      },
+      {
+        name: "Net Income/Employee",
+        year2017: '(£42.814)',
+        year2018: "(£52.871)",
+        chart: [10, 50, -20, 30, 10],
+      }
+    ]
     return (
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">Key Financial Metrics</h5>
           <div className="scroll-table" style={{ height: 300 }}>
             <table className="table">
-              <tr  className="table-borderless">
-                <td className="text-light pl-0" ></td>
-                <td className="text-light"></td>
-                <td className="text-light">2017</td>
-                <td className="text-light">2018</td>
-              </tr>
-              <tr>
-                <td className="text-light pl-0">Turnovor</td>
-                <td className="text-light">£30.759.000</td>
-                <td className="text-light">£59.484.000</td>
-                <td className="text-light">Software</td>
-              </tr>
-              <tr>
-                <td className="text-light pl-0">Gress Profit Margin</td>
-                <td className="text-light">76,6%</td>
-                <td className="text-light">81,9%</td>
-                <td className="text-light">Software</td>
-              </tr>
-              <tr>
-                <td className="text-light pl-0">Net Income Morgin</td>
-                <td className="text-light">(80,9%)</td>
-                <td className="text-light">(66,1%)</td>
-                <td className="text-light">6 years</td>
-              </tr>
-              <tr>
-                <td className="text-light pl-0">Return on Equity</td>
-                <td className="text-light">(178,3%)</td>
-                <td className="text-light">(290,4%)</td>
-                <td className="text-light">5D1-1000</td>
-              </tr>
-              <tr>
-                <td className="text-light pl-0">Turnover/Employee</td>
-                <td className="text-light">£52.941</td>
-                <td className="text-light">£79,952</td>
-                <td className="text-light">Late growth stage</td>
-              </tr>
-              <tr>
-                <td className="text-light pl-0">Net Income/Employee</td>
-                <td className="text-light">(£42.814)</td>
-                <td className="text-light">(£52.871)</td>
-                <td className="text-light">Software</td>
-              </tr>
+              {growthSummary.map((data, index) => (
+                <tr key={index} className={index == 0 ? "table-borderless" : ""}>
+                  <td className="text-light pl-0 pr-5">{data.name}</td>
+                  <td className="text-light ">{data.year2017}</td>
+                  <td className="text-light pl-0">{data.year2018}</td>
+                  <td className="text-light pl-0">
+                    <HighchartsReact
+                      highcharts={Highcharts}
+                      options={columnOptions(data.chart, "area", 70)}
+                    />
+                  </td>
+                </tr>
+              ))}
             </table>
           </div>
         </div>
@@ -60,3 +80,5 @@ class FinancialMetrics extends React.Component {
   }
 }
 export default FinancialMetrics
+
+
